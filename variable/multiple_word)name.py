ab2e3b323770bae2615f96f4b8cf3282ef2dda70
pornphawit <pornphawit.pe@ku.th>
2021-01-10 11:09:08 +0700

    Add 3 file this project

---
+++ variable/multiple_word)name.py
@@ -0,0 +1,8 @@
+# Camel Case
+myVariableName = "Pure Pornphawit"
+
+# Pascal Case
+MyVariableName = "Pure Pornphawit"
+
+# Snake case
+my_variable_name = "Pure Pornphawit"
---
+++ variable/name.py
@@ -0,0 +1,15 @@
+# สามารถประกาศได้
+myvar = "pure"
+my_var = "pure"
+_my_var_ = "pure"
+myVar = "pure"
+MYVAR = "pure"
+myvar2 = "pure"
+
+# ไม่สามารถประกาศได้
+# 2myvar = 'pure'
+# my-var = 'pure'
+# my var = 'pure'
+
+
+PI = 3.14
